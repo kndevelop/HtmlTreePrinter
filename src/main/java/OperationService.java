c67bcc0538cb8cee33152ea3d848897791bd326b
kndevelop <kn02sb04@gmail.com>
2026-06-07 15:00:06 +0900

    検証

--- src/main/java/OperationService.java
+++ src/main/java/OperationService.java
@@ -54,6 +54,9 @@ public class OperationService {
         String selector
     ) {
         System.out.println(page.url());
+        System.out.println(page.title());
+        System.out.println(page.content().contains("id_email"));
+        System.out.println(page.locator("#id_email").count());
         page.waitForSelector(selector);
         page.locator(selector)
             .fill(env != null
